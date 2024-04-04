cb88a27b6fb37719ee59dee349774db0e4567e95
craigbsch <150191280+craigbsch@users.noreply.github.com>
2024-04-03 22:35:49 -0400

    Added basic login form

--- react-frontend/src/components/Login.js
+++ react-frontend/src/components/Login.js
@@ -1,12 +1,27 @@
-import React from "react";
-//can change any of this
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const Login = () => {
 	return (
-		<div>
-			<h1>Login with UMass credentials</h1>
-			{/* add login form here */}
-		</div>
+		<Form>
+			<h2>Login with UMass credentials</h2>
+			<Form.Group className='mb-3' controlId='formBasicEmail'>
+				<Form.Label>Email address</Form.Label>
+				<Form.Control type='email' placeholder='Enter email' />
+				<Form.Text className='text-muted'>
+					We'll never share your email with anyone else.
+				</Form.Text>
+			</Form.Group>
+
+			<Form.Group className='mb-3' controlId='formBasicPassword'>
+				<Form.Label>Password</Form.Label>
+				<Form.Control type='password' placeholder='Password' />
+			</Form.Group>
+			<Form.Group className='mb-3' controlId='formBasicCheckbox'>
+				<Form.Check type='checkbox' label='Remember me' />
+			</Form.Group>
+			<Button variant='primary'>Submit</Button>
+		</Form>
 	);
 };
 
