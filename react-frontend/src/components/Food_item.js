c5bb697ac4ce3c408dfc6489efd969b47b1b344f
samr1139 <96459508+samr1139@users.noreply.github.com>
2024-04-03 20:04:39 -0400

    Food_item.js

--- react-frontend/src/components/Food_item.js
+++ react-frontend/src/components/Food_item.js
@@ -1,14 +1,14 @@
 import React from 'react';
-//can change any of this
+//can change any of this. very rough
 
-const Food_item = ({ meal, onSelect, onRemove, isSelected }) => {
+const FoodItem = ({ meal, onSelect, onRemove, isSelected }) => {
   return (
     <li>
-      <span>{meal.name}</span>
-      <button onClick={() => onSelect(meal)}>+</button>
-      {isSelected && <button onClick={() => onRemove(meal)}>-</button>}
+      <span>{food.name}</span>
+      <button onClick={() => onSelect(food)}>+</button>
+      {isSelected && <button onClick={() => onRemove(food)}>-</button>}
     </li>
   );
 };
 
-export default Food_item;
+export default FoodItem;
