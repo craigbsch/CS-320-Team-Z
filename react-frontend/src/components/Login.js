5c04e3d72a2fc0d9ba6e8f1c612986522e13f32e
craigbsch <150191280+craigbsch@users.noreply.github.com>
2024-04-08 14:33:36 -0400

    fix import error from merge

--- react-frontend/src/components/Login.js
+++ react-frontend/src/components/Login.js
@@ -2,7 +2,6 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container"; // Import Container
-import Container from "react-bootstrap/Container";
 
 // Login component
 const Login = () => {
