980777cb145485ed956d8c7c65c0009a8039693c
craigbsch <150191280+craigbsch@users.noreply.github.com>
2024-04-10 19:34:24 -0400

    Displays food items in menu component

--- react-frontend/src/pages/ViewMenuPage.js
+++ react-frontend/src/pages/ViewMenuPage.js
@@ -1,10 +1,31 @@
 import DiningHallSelector from "../components/DiningHallSelector";
 import Menu from "../components/Menu";
 import Container from "react-bootstrap/Container";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setMenu } from "../redux/menuSlice";
 
 const ViewMenuPage = () => {
 	const [hall, setHall] = useState("Select hall");
+	const date = new Date();
+	const [day, setDay] = useState(date.getDay());
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		axios
+			.get(
+				`https://dininginfobackend.azurewebsites.net/menu?dining_hall=${hall}&date_served=${
+					date.toISOString().split("T")[0]
+				}`
+			)
+			.then((response) => {
+				dispatch(setMenu(response.data));
+			})
+			.catch((error) => {
+				console.error("Error fetching tasks:", error);
+			});
+	}, [date, hall, dispatch]);
 
 	return (
 		<Container>
