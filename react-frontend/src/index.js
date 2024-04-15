6cdf92824cfe0a46996b7d65ae465da57ecfa242
Owen DePoint <owen.depoint@gmail.com>
2024-04-15 12:10:58 -0400

    started implementation of auth, instant redirect

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -9,21 +9,31 @@ import ViewMenuPage from "./pages/ViewMenuPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import AutoLogin from "./components/AutoLogin.js";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
+<Auth0Provider
+    domain="dev-dvb6li7z8kj02il0.us.auth0.com"
+    clientId="VhHtVyvfRtjN3ldu7gY8D2uAOuHD2pTV"
+    authorizationParams={{
+      redirect_uri: window.location.origin + '/viewMenu'
+    }}
+  >
 		<Provider store={store}>
 			<BrowserRouter>
 				<Routes>
+					<Route index element={<AutoLogin />} /> 
 					<Route path='/' element={<Layout />}>
-						<Route index element={<LoginPage />} />
-						<Route path='viewMenu' element={<ViewMenuPage />} />
+					<Route path='viewMenu' element={<ViewMenuPage />} />
 					</Route>
 				</Routes>
 			</BrowserRouter>
 		</Provider>
-	</React.StrictMode>
+	</Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
