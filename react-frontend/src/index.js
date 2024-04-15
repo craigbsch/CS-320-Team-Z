cdaf946cd90718e276af2a76133a8a2aab2de0a2
Owen DePoint <owen.depoint@gmail.com>
2024-04-15 16:08:08 -0400

    protected routes

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -4,36 +4,36 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
-import LoginPage from "./pages/LoginPage";
 import ViewMenuPage from "./pages/ViewMenuPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
-import { createRoot } from 'react-dom/client';
-import { Auth0Provider } from '@auth0/auth0-react';
 import AutoLogin from "./components/AutoLogin.js";
+import ProtectedRoute from "./components/ProtectedRoute.js";
+import Auth0ProviderWithRedirectCallback from "./components/Auth0ProviderWithRedirectCallback.js";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<Auth0Provider
-    domain="dev-dvb6li7z8kj02il0.us.auth0.com"
-    clientId="VhHtVyvfRtjN3ldu7gY8D2uAOuHD2pTV"
-    authorizationParams={{
-      redirect_uri: window.location.origin + '/viewMenu'
-    }}
-  >
-		<Provider store={store}>
-			<BrowserRouter>
-				<Routes>
-					<Route index element={<AutoLogin />} /> 
-					<Route path='/' element={<Layout />}>
-					<Route path='viewMenu' element={<ViewMenuPage />} />
-					</Route>
-				</Routes>
-			</BrowserRouter>
-		</Provider>
-	</Auth0Provider>
+
+	<Provider store={store}>
+	<BrowserRouter>
+		<Auth0ProviderWithRedirectCallback
+			domain="dev-dvb6li7z8kj02il0.us.auth0.com"
+			clientId="VhHtVyvfRtjN3ldu7gY8D2uAOuHD2pTV"
+			authorizationParams={{
+			redirect_uri: window.location.origin + '/viewMenu'
+			}}
+		>
+		<Routes>
+			<Route index element={<AutoLogin />} /> 
+			<Route path='/' element={<ProtectedRoute component = {Layout} />}>
+			<Route path='viewMenu' element={<ProtectedRoute component = {ViewMenuPage} />} />
+			</Route>
+		</Routes>
+		</Auth0ProviderWithRedirectCallback>
+	</BrowserRouter>
+	</Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
