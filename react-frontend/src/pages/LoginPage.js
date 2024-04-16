7e9064b6b3011049c1ee2468c0c834080c49c3ff
samr1139 <96459508+samr1139@users.noreply.github.com>
2024-04-16 15:39:29 -0400

    Update LoginPage.js

--- react-frontend/src/pages/LoginPage.js
+++ react-frontend/src/pages/LoginPage.js
@@ -1,7 +1,13 @@
-import { Login } from "../components";
-
 const LoginPage = () => {
-	return <Login />;
+	return (
+		<Container
+			className='d-flex justify-content-center'
+			style={{ minHeight: "100vh", paddingTop: "10vh" }}
+		>
+			<div style={{ minWidth: "320px", maxWidth: "400px", width: "100%" }}>
+				<Login />
+			</div>
+		</Container>
+	);
 };
-
 export default LoginPage;
