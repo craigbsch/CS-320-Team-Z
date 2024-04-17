fbb63e96648613b52d0225f41025e2f77ddd7ff0
Owen DePoint <owen.depoint@gmail.com>
2024-04-17 10:14:52 -0400

    update navbar with profile component

--- react-frontend/src/components/Navbar.js
+++ react-frontend/src/components/Navbar.js
@@ -1,26 +1,23 @@
-// importing necessary components from the React Bootstrap library
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
+import React from "react";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import LogoutButton from "../components/LogoutButton";
 import Profile from "./Profile";
+import "../styling/Navbar.css";
 
 //functional component for the navigation bar
 const NavBar = () => {
 	//rendering for the navigation bar
-	return (
-		<Navbar style={{ backgroundColor: "#6C1515" }}>
-			<Container style={{ display: "flex", justifyContent: "center" }}>
-				<Navbar.Brand>
-					<h1 style={{ color: "white" }}>Nutrition Calc</h1>
-				</Navbar.Brand>
-				<LogoutButton></LogoutButton>
-				<Profile></Profile>
-			</Container>
-		</Navbar>
-	);
+    return (
+        <Navbar className="navbar">
+            <div className="brand-container">
+                <Navbar.Brand>
+                    <h1 className="brand-title">Nutrition Calc</h1>
+                </Navbar.Brand>
+            </div>
+            <div className="profile-container">
+                <Profile />
+            </div>
+        </Navbar>
+    );
 };
 
-//Exporting the NavBar component to be used elsewhere in the app
 export default NavBar;
--- react-frontend/src/components/Profile.js
+++ react-frontend/src/components/Profile.js
@@ -1,23 +1,46 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Dropdown } from 'react-bootstrap';
+import "../styling/Profile.css"
 
 const Profile = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
+  const { user, isAuthenticated, isLoading, logout } = useAuth0();
 
   if (isLoading) {
     return <div>Loading ...</div>;
   }
 
-  console.log(user)
-  return (
-    isAuthenticated && (
-      <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.height}</p>
+  if (!isAuthenticated) {
+    return null;
+  }
 
-      </div>
-    )
+  const handleLogout = () => logout({ returnTo: window.location.origin });
+
+  return (
+    <Dropdown align="end">
+      <Dropdown.Toggle 
+        id="dropdown-custom-components" 
+        style={{ background: 'none', border: 'none', cursor: 'pointer' }}
+        as="a"
+      >
+        <img 
+          src={user.picture} 
+          alt="Profile" 
+          style={{ borderRadius: '50%', width: '50px', height: '50px' }} 
+        />
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu className="dropdown-menu-right" align="end">
+        <Dropdown.ItemText>{user.email}</Dropdown.ItemText>
+        <Dropdown.Divider />
+        <Dropdown.Item eventKey="1">My plan</Dropdown.Item>
+        <Dropdown.Item eventKey="2">Settings</Dropdown.Item>
+        <Dropdown.Divider />
+        <Dropdown.Item eventKey="3" onClick={handleLogout}>
+          Log out
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
   );
 };
 
