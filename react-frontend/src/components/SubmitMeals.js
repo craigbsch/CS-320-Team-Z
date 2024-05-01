179119fc8cb8139330566ae4cf9417cb746dd2a0
craigbsch <150191280+craigbsch@users.noreply.github.com>
2024-05-01 19:57:07 -0400

    Added submit functionality

--- react-frontend/src/components/SubmitMeals.js
+++ react-frontend/src/components/SubmitMeals.js
@@ -2,10 +2,18 @@ import React, { useState } from "react";
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import axiosFASTAPI from "../api/common";
+import { useAuth0 } from "@auth0/auth0-react";
+import Spinner from "react-bootstrap/Spinner";
+import Alert from "react-bootstrap/Alert";
 
 const SubmitMeals = (props) => {
 	const [visible, setVisibility] = useState(false);
-	const [items, setItems] = useState({}); // No types since this isn't TS
+	const [items, setItems] = useState({});
+	const [isLoadingSubmit, setLoadingSubmit] = useState(false);
+	const [submitStatus, setSubmitStatus] = useState(null); // 'success', 'error', or null
+	const { user, isAuthenticated, isLoading, getAccessTokenSilently, logout } =
+		useAuth0();
 
 	const processItems = () => {
 		const tempItems = {};
@@ -15,6 +23,41 @@ const SubmitMeals = (props) => {
 				: (tempItems[m.meal_name] = 1)
 		);
 		setItems(tempItems);
+	};
+
+	const submit = async () => {
+		setLoadingSubmit(true);
+		setSubmitStatus(null);
+		try {
+			const accessToken = await getAccessTokenSilently();
+			const response = await axiosFASTAPI.post(
+				"/nutrition/api/update_meals",
+				props.selectedItems,
+				{
+					headers: {
+						Authorization: `Bearer ${accessToken}`,
+						"Content-Type": "application/json",
+					},
+				}
+			);
+			console.log(response.data);
+			setVisibility(false);
+			setSubmitStatus({ type: "success", message: "Submission Successful!" });
+			setLoadingSubmit(false);
+			setTimeout(() => setSubmitStatus(null), 3000);
+
+			const refreshToken = await getAccessTokenSilently({
+				cacheMode: "off",
+				grant: "refresh_token",
+				detailedResponse: true,
+			});
+		} catch (error) {
+			console.error("Error submitting user metadata:", error);
+			setSubmitStatus({ type: "danger", message: "Error submitting data!" });
+			setTimeout(() => setSubmitStatus(null), 3000);
+		} finally {
+			setLoadingSubmit(false);
+		}
 	};
 
 	return props.selectedItems.length > 0 ? (
@@ -42,22 +85,51 @@ const SubmitMeals = (props) => {
 							</tr>
 						</thead>
 						<tbody>
-							{Object.keys(items).map((item, i) => (
+							{Object.keys(items).map((key, i) => (
 								<React.Fragment key={`item-${i}`}>
 									<tr>
-										<td>{item}</td>
-										<td>{items[item]}</td>
+										<td>{key}</td>
+										<td>{items[key]}</td>
 									</tr>
 								</React.Fragment>
 							))}
 						</tbody>
 					</Table>
+					{isLoadingSubmit && (
+						<div
+							style={{
+								position: "fixed",
+								top: "50%",
+								left: "50%",
+								transform: "translate(-50%, -50%)",
+								zIndex: 1060, // Ensure this is above the modal z-index
+							}}
+						>
+							<Spinner animation='border' variant='primary' />
+						</div>
+					)}
+					{submitStatus && (
+						<Alert
+							variant={submitStatus.type}
+							className={`custom-alert custom-alert-${submitStatus.type}`}
+							style={{
+								position: "fixed",
+								bottom: "10px",
+								left: "10%",
+								right: "10%",
+								textAlign: "center",
+								zIndex: 1060,
+							}}
+						>
+							{submitStatus.message}
+						</Alert>
+					)}
 				</Modal.Body>
 				<Modal.Footer>
 					<Button variant='danger' onClick={() => setVisibility(false)}>
 						Cancel
 					</Button>
-					<Button variant='success' onClick={() => setVisibility(false)}>
+					<Button variant='success' onClick={() => submit()}>
 						Submit
 					</Button>
 				</Modal.Footer>
