e11ab4d937a938eeb493345d270a723c84909e97
samr1139 <96459508+samr1139@users.noreply.github.com>
2024-05-08 18:25:07 -0400

    Added comments in Auth0ProviderWithRedirectCallback.js

--- react-frontend/src/components/Auth0ProviderWithRedirectCallback.js
+++ react-frontend/src/components/Auth0ProviderWithRedirectCallback.js
@@ -1,6 +1,8 @@
+//Importing necessary dependencies from react-router and Auth0
 import { useNavigate } from "react-router";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+// Defining a custom Auth0Provider component that includes a redirect callback
 const Auth0ProviderWithRedirectCallback = ({ children, ...props }) => {
 	const navigate = useNavigate();
 	const onRedirectCallback = (appState) => {
