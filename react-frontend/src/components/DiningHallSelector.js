c635f5bdc02c9ad520694921c1ef58c9ea3236c0
samr1139 <96459508+samr1139@users.noreply.github.com>
2024-05-08 18:28:52 -0400

    Comments in DiningHallSelector.js

--- react-frontend/src/components/DiningHallSelector.js
+++ react-frontend/src/components/DiningHallSelector.js
@@ -1,10 +1,14 @@
+// Importing necessary components from react-bootstrap
 import Container from "react-bootstrap/Container";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
 
+// Defining a DiningHallSelector component
 const DiningHallSelector = (props) => {
+	// Array containing the names of dining halls
 	const halls = ["Franklin", "Berkshire", "Worcester", "Hampshire"];
 
+	// Rendering the component
 	return (
 		<Container className='d-flex justify-content-center align-items-center'>
 			Select your dining hall:
