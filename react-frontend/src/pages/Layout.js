f113e2bd81e74bf608c9947313708e4dd9697643
samr1139 <96459508+samr1139@users.noreply.github.com>
2024-05-08 18:54:46 -0400

    Comments in Layout.js

--- react-frontend/src/pages/Layout.js
+++ react-frontend/src/pages/Layout.js
@@ -2,7 +2,9 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { NavBar } from "../components";
 
+// Define Layout component
 const Layout = () => {
+	// Render Layout component
 	return (
 		<>
 			<NavBar />
