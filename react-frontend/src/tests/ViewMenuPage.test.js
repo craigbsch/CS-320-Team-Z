8a66835affdf4c6c5a69515c342a03f39bc70bea
craigbsch <150191280+craigbsch@users.noreply.github.com>
2024-05-15 15:44:11 -0400

    preliminary ViewMenuPage Tests

--- react-frontend/src/tests/ViewMenuPage.test.js
+++ react-frontend/src/tests/ViewMenuPage.test.js
@@ -1,21 +1,61 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, cleanup, fireEvent } from "@testing-library/react";
 import { ViewMenuPage } from "../pages";
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
 
-render(
-	<Provider store={store}>
-		<ViewMenuPage />
-	</Provider>
-);
+const rend = () =>
+	render(
+		<Provider store={store}>
+			<ViewMenuPage />
+		</Provider>
+	);
 
 describe("ViewMenuPage Tests", () => {
+	const originalConsoleError = console.error;
+	beforeAll(() => {
+		console.error = jest.fn();
+	});
+
+	afterAll(() => {
+		cleanup();
+		console.error = originalConsoleError;
+	});
+
 	it("renders header", () => {
+		rend();
 		expect(screen.getByText("View Menus")).toBeInTheDocument();
 	});
 
 	it("renders Dining Hall Selector", () => {
-		expect(screen.getByText("Select your dining hall:")).toBeInTheDocument();
+		rend();
+		expect(
+			screen.getByRole("button", {
+				name: /Select hall/i,
+			})
+		).toBeInTheDocument();
+	});
+
+	it("renders Date Selector", () => {
+		rend();
+		const buttons = screen.getAllByRole("button");
+		expect(
+			buttons.find((button) => /^\d{4}-\d{2}-\d{2}$/.test(button.textContent))
+		).toBeInTheDocument();
+	});
+
+	it.skip("renders Menu", async () => {
+		rend();
+		fireEvent.click(
+			screen.getByRole("button", {
+				name: /Select hall/i,
+			})
+		);
+		fireEvent.click(
+			screen.getByRole("button", {
+				name: /Worcester/i,
+			})
+		);
+		expect(await screen.findByRole("table")).toBeInTheDocument();
 	});
 });
