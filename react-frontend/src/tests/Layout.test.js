f901107c15d0f3fe651fbb4b3841e45a4c0551e2
craigbsch <150191280+craigbsch@users.noreply.github.com>
2024-05-15 16:00:44 -0400

    Adds layout tests

--- react-frontend/src/tests/Layout.test.js
+++ react-frontend/src/tests/Layout.test.js
@@ -1,8 +1,48 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import {
+	render,
+	screen,
+	cleanup,
+	getAllByRole,
+	getByRole,
+} from "@testing-library/react";
 import { Layout } from "../pages";
+import { BrowserRouter } from "react-router-dom";
 
-it.skip("renders Layout Text", () => {
-	render(<Layout />);
-	expect(screen.getByText("Nutrition Calc")).toBeInTheDocument();
+const rend = () =>
+	render(
+		<BrowserRouter>
+			<Layout />
+		</BrowserRouter>
+	);
+
+describe("Layout Tests", () => {
+	const originalConsoleError = console.error;
+	beforeAll(() => {
+		console.error = jest.fn();
+	});
+
+	afterAll(() => {
+		cleanup();
+		console.error = originalConsoleError;
+	});
+
+	it("renders header Text", () => {
+		rend();
+		expect(screen.getByText("Nutrition Calc")).toBeInTheDocument();
+	});
+
+	it("renders Back Button", () => {
+		rend();
+		expect(
+			screen.getByRole("button", { class: "back-button btn btn-link" })
+		).toBeInTheDocument();
+	});
+
+	it.skip("renders Profile button", () => {
+		rend();
+		expect(
+			screen.getByRole("generic", { name: "profile-container" })
+		).toBeInTheDocument();
+	});
 });
